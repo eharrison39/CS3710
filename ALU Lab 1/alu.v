44ca26f81ecc26c4a796be256615dc4cec30dc8c
Elden Harrison <u1358661@utah.edu>
2025-09-02 16:36:42 -0600

    Did more operators
    We have questions on all sh commands, cmp flags, and add flags.

--- ALU Lab 1/alu.v
+++ ALU Lab 1/alu.v
@@ -27,33 +27,33 @@ output reg [4:0] Flags;
 
 
 
-parameter ADDU = 8'b0000 0110;
 parameter ADD = 8'b0000 0101;
-parameter ADDI = 8'b0101 ;
-parameter ADDUI = 8'b0110 ;
+parameter ADDI = 8'b0101 0000;
+parameter ADDU = 8'b0000 0110;
+parameter ADDUI = 8'b0110 0000;
 parameter ADDC = 8'b0000 0111;
-parameter ADDCU = 8'b ;
-parameter ADDCI = 8'b0111 ;
-parameter ADDCUI = 8'b ;
+parameter ADDCI = 8'b0111 0000;
+parameter ADDCU = 8'b0000 1111; ////
+parameter ADDCUI = 8'1111 0000; ////
 
 parameter SUB = 8'b0000 1001;
-parameter SUBI = 8'b1001 ;
+parameter SUBI = 8'b1001 0000;
 
 parameter CMP = 8'b0000 1011;
-parameter CMPI = 8'b1011 ;
-parameter CMPUI = 8'b ;
+parameter CMPI = 8'b1011 0000;
+parameter CMPUI = 8'b0100 0000; ////
 
 parameter AND = 8'b0000 0001;
 parameter OR = 8'b0000 0010;
 parameter XOR = 8'b0000 0011;
-parameter NOT = 8'b0000 ;
+parameter NOT = 8'b0000 0100; ////
 
 parameter LSH = 8'b1000 0100;
 parameter LSHI = 8'b1000 000s;
-parameter RSH = 8'b ;
-parameter RSHI = 8'b ;
-parameter ALSH = 8'b ;
-parameter ARSH = 8'b ;
+parameter RSH = 8'b ; ////
+parameter RSHI = 8'b ; ////
+parameter ALSH = 8'b ; ////
+parameter ARSH = 8'b ; ////
 parameter WAIT = 8'b0000 0000;
 
 
@@ -64,83 +64,172 @@ begin
 	case (Opcode)
 	ADDU:
 		begin
-			{Flags[3], C} = A + B;
-			// perhaps if ({Flags[3], C} == 5'b00000) ....
+			{Flags[4], C} = A + B;
 			
 			if (C == 16'b0000 0000 0000 0000)
-				Flags[4] = 1'b1; 
+				Flags[1] = 1'b1; 
 			else 
-				Flags[4] = 1'b0;
-				
-			Flags[2:0] = 3'b000;
-		end
-	ADD:
-		begin
-		C = A + B;
-		if (C == 4'b0000) Flags[4] = 1'b1;
-		else Flags[4] = 1'b0;
-		if( (~A[3] & ~B[3] & C[3]) | (A[3] & B[3] & ~C[3]) ) Flags[2] = 1'b1;
-		else Flags[2] = 1'b0;
-		Flags[1:0] = 2'b00; Flags[3] = 1'b0;
-
-		end
-	ADDI:
-		begin
-		
+				Flags[1] = 1'b0;
+			
+			Flags[0] = 1'b0;
+			Flags[3:2] = 2'b00;
 		end
 	ADDUI:
 		begin
-		
+			{Flags[4], C} = A + B;
+			
+			if (C == 16'b0000 0000 0000 0000)
+				Flags[1] = 1'b1; 
+			else 
+				Flags[1] = 1'b0;
+			
+			Flags[0] = 1'b0;
+			Flags[3:2] = 2'b00;
+		end
+	ADD:
+		begin
+			C = A + B;
+			if (C == 16'b0000 0000 0000 0000)
+				Flags[1] = 1'b1;
+			else
+				Flags[1] = 1'b0;
+				
+			if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) )
+				Flags[2] = 1'b1;
+			else
+				Flags[2] = 1'b0;
+				
+			Flags[3:4] = 2'b00; Flags[0] = 1'b0;
+		end
+	ADDI:
+		begin
+			C = A + B;
+			if (C == 16'b0000 0000 0000 0000)
+				Flags[1] = 1'b1;
+			else
+				Flags[1] = 1'b0;
+				
+			if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) )
+				Flags[2] = 1'b1;
+			else
+				Flags[2] = 1'b0;
+				
+			Flags[4:3] = 2'b00; Flags[0] = 1'b0;
 		end
 	ADDC:
 		begin
-		
+			C = A + B + 1'b1;
+			
+			if (C == 16'b0000 0000 0000 0000)
+				Flags[1] = 1'b1;
+			else
+				Flags[1] = 1'b0;
+				
+			if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) )
+				Flags[2] = 1'b1;
+			else
+				Flags[2] = 1'b0;
+				
+			Flags[4:3] = 2'b00; Flags[0] = 1'b0;
 		end
 	ADDCU:
 		begin
-		
+			{Flags[4], C} = A + B + 1'b1;
+			
+			if (C == 16'b0000 0000 0000 0000)
+				Flags[1] = 1'b1; 
+			else 
+				Flags[1] = 1'b0;
+			
+			Flags[0] = 1'b0;
+			Flags[3:2] = 2'b00;
 		end
 	ADDCUI:
 		begin
-		
+			{Flags[4], C} = A + B + 1'b1;
+			
+			if (C == 16'b0000 0000 0000 0000)
+				Flags[1] = 1'b1; 
+			else 
+				Flags[1] = 1'b0;
+			
+			Flags[0] = 1'b0;
+			Flags[3:2] = 2'b00;
 		end
 		
 	//////////////////
 	SUB:
 		begin
-		C = A - B;
-		if (C == 4'b0000) Flags[4] = 1'b1;
-		else Flags[4] = 1'b0;
-		if( (~A[3] & ~B[3] & C[3]) | (A[3] & B[3] & ~C[3]) ) Flags[2] = 1'b1;
-		else Flags[2] = 1'b0;
-		Flags[1:0] = 2'b00; Flags[3] = 1'b0;
+			C = A - B;
+			if (C == 16'b0000 0000 0000 0000)
+				Flags[3] = 1'b1;
+			else
+				Flags[3] = 1'b0;
+				
+			if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) )
+				Flags[2] = 1'b1;
+			else
+				Flags[2] = 1'b0;
+			Flags[1:0] = 2'b00; Flags[4] = 1'b0;
 		end
 		
 	SUBI:
 		begin
-		
+			C = A - B;
+			if (C == 16'b0000 0000 0000 0000)
+				Flags[3] = 1'b1;
+			else
+				Flags[3] = 1'b0;
+				
+			if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) )
+				Flags[2] = 1'b1;
+			else
+				Flags[2] = 1'b0;
+			Flags[1:0] = 2'b00; Flags[4] = 1'b0;
 		end
 		
 	//////////////////
 	CMP:
 		begin
-		if( $signed(A) < $signed(B) ) Flags[1:0] = 2'b11;
-		else Flags[1:0] = 2'b00;
-		C = 4'b0000;
-		Flags[4:2] = 3'b000;
-		// both positive or both negative
-		/*if( A[3] == B[3] )
-		begin
-			if (A < B) Flags[1:0] = 2'b11;
-			else Flags[1:0] = 2'b00;
-		end
-		else if (A[3] == 1'b0) Flags[1:0] = 2'b00;
-		else Flags[1:0] = 2'b01;
-		Flags[4:2] = 3'b000;
-		
-		// C = ?? if I don;t specify, then I'm in trouble.
-		C = 4'b0000;
-		*/
+			if( $signed(A) < $signed(B) ) begin
+				Flags[0] = 1'b1;
+				Flags[3] = 1'b1;
+			end
+			else begin
+				Flags[0] = 1'b0;
+				Flags[3] = 1'b0;
+			end
+				
+			C = 16'b0000 0000 0000 0000;
+			Flags[4:2] = 3'b000;
+			
+			// both positive or both negative
+			if( A[15] == B[15] ) begin
+				if (A < B) begin
+					Flags[0] = 1'b1;
+					Flags[3] = 1'b1;
+				end
+				else begin
+					Flags[0] = 1'b0;
+					Flags[3] = 1'b0;
+				end
+			end
+			else if (A[15] == 1'b0)
+				if (A < B) begin
+					Flags[0] = 1'b1;
+					Flags[3] = 1'b1;
+				end
+				else begin
+					Flags[0] = 1'b0;
+					Flags[3] = 1'b0;
+				end
+				
+			Flags[2:1] = 2'b00;
+			Flags[4] = 1'b0;
+			
+			// C = ?? if I don;t specify, then I'm in trouble.
+			C = 16'b0000 0000 0000 0000;
+			
 		end
 	CMPI:
 		begin
@@ -154,19 +243,19 @@ begin
 	//////////////////
 	AND:
 		begin
-		
+			C = A & B;
 		end
 	OR:
 		begin
-		
+			C = A | B;
 		end
 	XOR:
 		begin
-		
+			C = A ^ B;
 		end
 	NOT:
 		begin
-		
+			C = ~A;
 		end
 		
 	//////////////////
@@ -201,8 +290,8 @@ begin
 		
 	default: 
 		begin
-			C = 4'b0000;
-			Flags = 5'b00000;
+			C = 16'b0000 0000 0000 0000;
+			Flags = 5'b0000 0;
 		end
 	endcase
 end
