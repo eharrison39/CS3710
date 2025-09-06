15847a1ada69ece7b15c930d19c88b2f8aa671d2
u1359199 <156398082+u1359199@users.noreply.github.com>
2025-09-05 18:34:31 -0600

    Revised Comments and Adding correct file

--- ALU Lab 1/alu.v
+++ ALU Lab 1/alu.v
@@ -1,6 +1,6 @@
+
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
 // Engineer: 		 Luke Jones, David Brown, Elden Harrison
 // 
 // Create Date:    12:54:08 08/29/2025 
@@ -13,7 +13,7 @@
 //
 // Dependencies: 
 //
-// Revision: 
+// Revision: 	9/4/2025
 // Revision 0.01 - File Created
 // Additional Comments: 
 //
@@ -27,102 +27,66 @@ output reg [15:0] C;
 output reg [4:0] Flags;
 
 
-
+// Add Opcodes
 parameter ADD = 5'b0_0101;
-// parameter ADDI = 8'b0101 0000;
 parameter ADDU = 5'b0_0110;
-// parameter ADDUI = 8'b0110 0000;
 parameter ADDC = 5'b0_0111;
-// parameter ADDCI = 8'b0111 0000;
-parameter ADDCU = 5'b0_1111; ////
-// parameter ADDCUI = 8'1111 0000; ////
+parameter ADDCU = 5'b0_1111;
 
+// Subtraction Opcode
 parameter SUB = 5'b0_1001;
-// parameter SUBI = 8'b1001 0000;
 
+// Comparison Opcode
 parameter CMP = 5'b0_1011;
-// parameter CMPI = 8'b1011 0000;
-// parameter CMPU = 8'b0100 0000; ////
 
+// Bit Logic Opcodes
 parameter AND = 5'b0_0001;
 parameter OR = 5'b0_0010;
 parameter XOR = 5'b0_0011;
-parameter NOT = 5'b0_0100; ////
+parameter NOT = 5'b0_0100;
 
+// Shifting Opcodes
 parameter LSH = 8'b0_1100;
-// parameter LSHI = 8'b1000 000s;
-parameter RSH = 8'b1_0011 ; ////
-// parameter RSHI = 8'b ; ////
-// parameter ALSH = 8'b0 1110 ; ////
-parameter ARSH = 8'b1_0111 ; ////
-//parameter WAIT = 8'b0 0000;
+parameter RSH = 8'b1_0011 ;
+parameter ARSH = 8'b1_0111 ;
 
 
+// Begin opcodes case statement
 always @(A, B, Opcode, Cin)
 begin
 	case (Opcode)
+	
+	// Regular add without setting flags
 	ADDU:
 		begin
 			C = A + B;
-			
-//			if (C == 16'b0000 0000 0000 0000)
-//				Flags[1] = 1'b1; 
-//			else 
-//				Flags[1] = 1'b0;
-//			
-//			Flags[0] = 1'b0;
 			Flags[4:0] = 5'b0_0000;
 		end
-//	ADDUI:
-//		begin
-//			{Flags[4], C} = A + B;
-			
-//			if (C == 16'b0000 0000 0000 0000)
-//				Flags[1] = 1'b1; 
-//			else 
-//				Flags[1] = 1'b0;
-			
-//			Flags[0] = 1'b0;
-//			Flags[3:2] = 2'b00;
-//		end
+	
+	// Regular add. Sets the Carry bit, Overflow bit, Negative bit, and the Zero bit
 	ADD:
 		begin
+			// Set C and Carry bit
 			{Flags[4], C} = A + B;
-			//C = A + B;
+			
+			// Set Zero bit
 			if (C == 16'b0000_0000_0000_0000)
 				Flags[1] = 1'b1;
 			else
 				Flags[1] = 1'b0;
-//			if(A == B)
-//				Flags[1] = 1'b1;
-//			else
-//				Flags[1] = 1'b0;
-				
+			
+			// Set Overflow bit
 			if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) )
 				Flags[2] = 1'b1;
 			else
 				Flags[2] = 1'b0;
 				
-			Flags[3] = 1'b0; Flags[0] = C[15];
+			Flags[3] = 1'b0; Flags[0] = C[15]; // Set Negative bit
 		end
-//	ADDI:
-//		begin
-//			C = A + B;
-//			if (C == 16'b0000 0000 0000 0000)
-//				Flags[1] = 1'b1;
-//			else
-//				Flags[1] = 1'b0;
-//				
-//			if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) )
-//				Flags[2] = 1'b1;
-//			else
-//				Flags[2] = 1'b0;
-//				
-//			Flags[4:3] = 2'b00; Flags[0] = 1'b0;
-//		end
+	
+	// Functions and sets flags the same way as "ADD" but just adding a carry bit.
 	ADDC:
 		begin
-			//C = A + B + 1'b1;
 			{Flags[4], C} = A + B + Cin;
 			
 			if (C == 16'b0000_0000_0000_0000)
@@ -137,136 +101,59 @@ begin
 				
 			Flags[3] = 1'b0; Flags[0] = C[15];
 		end
+	
+	// Regular ADD carry but doesn't set the flag bits.
 	ADDCU:
 		begin
 			C = A + B + Cin;
-//			{Flags[4], C} = A + B + 1'b1;
-//			
-//			if (C == 16'b0000 0000 0000 0000)
-//				Flags[1] = 1'b1; 
-//			else 
-//				Flags[1] = 1'b0;
-//			
-//			Flags[0] = 1'b0;
 			Flags[4:0] = 5'b0_0000;
 		end
-//	ADDCUI:
-//		begin
-//			{Flags[4], C} = A + B + 1'b1;
-//			
-//			if (C == 16'b0000 0000 0000 0000)
-//				Flags[1] = 1'b1; 
-//			else 
-//				Flags[1] = 1'b0;
-//			
-//			Flags[0] = 1'b0;
-//			Flags[3:2] = 2'b00;
-//		end
-		
-	//////////////////
+	
+	// Regular subtract. Sets the Overflow bit, Negative bit, and the Zero bit
 	SUB:
 		begin
-<<<<<<< HEAD
-			//{Flags[4], C} = A - B;
 			C = A - B;
-=======
-			{Flags[4], C} = A - B;
->>>>>>> 7df1e42 (Fixed syntax errors)
+			
+			// Set Zero bit
 			if (C == 16'b0000_0000_0000_0000)
 				Flags[1] = 1'b1;
 			else
 				Flags[1] = 1'b0;
-				
+			
+			// Set Overflow bit
 			if( (A[15] ^ B[15]) & (A[15] ^ C[15]) )
 				Flags[2] = 1'b1;
 			else
 				Flags[2] = 1'b0;
 			
-			Flags[4:3] = 2'b00; Flags[0] = C[15];
+			Flags[4:3] = 2'b00; Flags[0] = C[15]; // Set Negative bit
 		end
-		
-//	SUBI:
-//		begin
-//			C = A - B;
-//			if (C == 16'b0000 0000 0000 0000)
-//				Flags[3] = 1'b1;
-//			else
-//				Flags[3] = 1'b0;
-//				
-//			if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) )
-//				Flags[2] = 1'b1;
-//			else
-//				Flags[2] = 1'b0;
-//			Flags[1:0] = 2'b00; Flags[4] = 1'b0;
-//		end
-		
-	//////////////////
+	
+	// Comparison instruction only sets flags. CMP sets the zero bit, low bit, and negative bit
 	CMP:
 		begin
-<<<<<<< HEAD
-			//Flags = 5'b00000;
-=======
-			Flags = 5'b00000;
->>>>>>> 7df1e42 (Fixed syntax errors)
+			// Set zero bit if A == B (A - B = 0)
 			if(A == B)
 				Flags[1] = 1'b1;
 			else
 				Flags[1] = 1'b0;
-<<<<<<< HEAD
-				
+			
+			// Set negative bit if signed a is less than signed b
 			if( $signed(A) < $signed(B) ) 
 				Flags[0] = 1'b1;
 			else 
 				Flags[0] = 1'b0;
-		
+			
+			// Set low bit if unsigned a is less than unsigned b
 			Flags[3] = (A < B);
-=======
->>>>>>> 7df1e42 (Fixed syntax errors)
-			//C = 16'b0000 0000 0000 0000;
-			//Flags[4:2] = 3'b000;
-			
-			
-			
-			// both positive or both negative 
-//			if( A[15] == B[15] ) begin
-//				if (A < B) begin
-//					Flags[0] = 1'b1;
-//					Flags[3] = 1'b1;
-//				end
-//				else begin
-//					Flags[0] = 1'b0;
-//					Flags[3] = 1'b0;
-//				end
-//			end
-//			else if (A[15] == 1'b0)
-//				if (A < B) begin
-//					Flags[0] = 1'b1;
-//					Flags[3] = 1'b1;
-//				end
-//				
-//		// N is set if signed a is less than b
-//				else begin
-//					Flags[0] = 1'b0;
-//					Flags[3] = 1'b0;
-//				end
 				
 			Flags[2] = 1'b0;
 			Flags[4] = 1'b0;
 			
-			// C = ?? if I don;t specify, then I'm in trouble.
-			C = 16'b0000_0000_0000_0000;
-			
+			C = 16'b0000_0000_0000_0000;	
 		end
-//	CMPI:
-//		begin
-//		
-//		end
-//	CMPU:
-//		begin
-//		
-//		end
-		
-	//////////////////
+
+// Bit Logic gates. They don't set any flag bits
 	AND:
 		begin
 			C = A & B;
@@ -288,39 +175,27 @@ begin
 			Flags = 5'b0_0000;
 		end
 		
-	//////////////////
+	// Shifting arithmetic. No flag bits set (Arithmetic and Logic Left Shift) - (They do the same thing)
 	LSH:
 		begin
 			C = A << B;
 			Flags = 5'b0_0000;
 		end
-//	LSHI:
-//		begin
-//		
-//		end
+
+	// Logical right shift
 	RSH:
 		begin
 			C = A >> B;
 			Flags = 5'b0_0000;
 		end
-//	RSHI:
-//		begin
-//		
-//		end
-//	ALSH:
-//		begin
-//		
-//		end
+	
+	// Arithmetic right shift
 	ARSH:
 		begin
 			C = $signed(A) >>> B;
 			Flags = 5'b0_0000;
 		end
-//	WAIT:
-//		begin
-//			
-//		end
-		
+
 	default: 
 		begin
 			C = 16'b0000_0000_0000_0000;
