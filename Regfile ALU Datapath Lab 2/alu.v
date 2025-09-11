550ed32f2b0c234356f240a7239e23a93947ba21
umyea33 <143153983+umyea33@users.noreply.github.com>
2025-09-11 16:40:59 -0600

    First commit to lab 2

---
+++ Regfile ALU Datapath Lab 2/alu.v
@@ -0,0 +1,321 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 		 Luke Jones, David Brown, Elden Harrison
+// 
+// Create Date:    12:54:08 08/29/2025 
+// Design Name: 
+// Module Name:    alu 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module alu( A, B, C, Opcode, Flags, Cin
+    );
+input [15:0] A, B;
+input [4:0] Opcode;
+input Cin;
+output reg [15:0] C;
+output reg [4:0] Flags;
+
+
+
+parameter ADD = 5'b0_0101;
+// parameter ADDI = 8'b0101 0000;
+parameter ADDU = 5'b0_0110;
+// parameter ADDUI = 8'b0110 0000;
+parameter ADDC = 5'b0_0111;
+// parameter ADDCI = 8'b0111 0000;
+parameter ADDCU = 5'b0_1111; ////
+// parameter ADDCUI = 8'1111 0000; ////
+
+parameter SUB = 5'b0_1001;
+// parameter SUBI = 8'b1001 0000;
+
+parameter CMP = 5'b0_1011;
+// parameter CMPI = 8'b1011 0000;
+// parameter CMPU = 8'b0100 0000; ////
+
+parameter AND = 5'b0_0001;
+parameter OR = 5'b0_0010;
+parameter XOR = 5'b0_0011;
+parameter NOT = 5'b0_0100; ////
+
+parameter LSH = 8'b0_1100;
+// parameter LSHI = 8'b1000 000s;
+parameter RSH = 8'b1_0011 ; ////
+// parameter RSHI = 8'b ; ////
+// parameter ALSH = 8'b0 1110 ; ////
+parameter ARSH = 8'b1_0111 ; ////
+//parameter WAIT = 8'b0 0000;
+
+
+always @(A, B, Opcode, Cin)
+begin
+	case (Opcode)
+	ADDU:
+		begin
+			C = A + B;
+			
+//			if (C == 16'b0000 0000 0000 0000)
+//				Flags[1] = 1'b1; 
+//			else 
+//				Flags[1] = 1'b0;
+//			
+//			Flags[0] = 1'b0;
+			Flags[4:0] = 5'b0_0000;
+		end
+//	ADDUI:
+//		begin
+//			{Flags[4], C} = A + B;
+			
+//			if (C == 16'b0000 0000 0000 0000)
+//				Flags[1] = 1'b1; 
+//			else 
+//				Flags[1] = 1'b0;
+			
+//			Flags[0] = 1'b0;
+//			Flags[3:2] = 2'b00;
+//		end
+	ADD:
+		begin
+			{Flags[4], C} = A + B;
+			//C = A + B;
+			if (C == 16'b0000_0000_0000_0000)
+				Flags[1] = 1'b1;
+			else
+				Flags[1] = 1'b0;
+//			if(A == B)
+//				Flags[1] = 1'b1;
+//			else
+//				Flags[1] = 1'b0;
+				
+			if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) )
+				Flags[2] = 1'b1;
+			else
+				Flags[2] = 1'b0;
+				
+			Flags[3] = 1'b0; Flags[0] = C[15];
+		end
+//	ADDI:
+//		begin
+//			C = A + B;
+//			if (C == 16'b0000 0000 0000 0000)
+//				Flags[1] = 1'b1;
+//			else
+//				Flags[1] = 1'b0;
+//				
+//			if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) )
+//				Flags[2] = 1'b1;
+//			else
+//				Flags[2] = 1'b0;
+//				
+//			Flags[4:3] = 2'b00; Flags[0] = 1'b0;
+//		end
+	ADDC:
+		begin
+			//C = A + B + 1'b1;
+			{Flags[4], C} = A + B + Cin;
+			
+			if (C == 16'b0000_0000_0000_0000)
+				Flags[1] = 1'b1;
+			else
+				Flags[1] = 1'b0;
+				
+			if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) )
+				Flags[2] = 1'b1;
+			else
+				Flags[2] = 1'b0;
+				
+			Flags[3] = 1'b0; Flags[0] = C[15];
+		end
+	ADDCU:
+		begin
+			C = A + B + Cin;
+//			{Flags[4], C} = A + B + 1'b1;
+//			
+//			if (C == 16'b0000 0000 0000 0000)
+//				Flags[1] = 1'b1; 
+//			else 
+//				Flags[1] = 1'b0;
+//			
+//			Flags[0] = 1'b0;
+			Flags[4:0] = 5'b0_0000;
+		end
+//	ADDCUI:
+//		begin
+//			{Flags[4], C} = A + B + 1'b1;
+//			
+//			if (C == 16'b0000 0000 0000 0000)
+//				Flags[1] = 1'b1; 
+//			else 
+//				Flags[1] = 1'b0;
+//			
+//			Flags[0] = 1'b0;
+//			Flags[3:2] = 2'b00;
+//		end
+		
+	//////////////////
+	SUB:
+		begin
+			//{Flags[4], C} = A - B;
+			C = A - B;
+			if (C == 16'b0000_0000_0000_0000)
+				Flags[1] = 1'b1;
+			else
+				Flags[1] = 1'b0;
+				
+			if( (A[15] ^ B[15]) & (A[15] ^ C[15]) )
+				Flags[2] = 1'b1;
+			else
+				Flags[2] = 1'b0;
+			
+			Flags[4:3] = 2'b00; Flags[0] = C[15];
+		end
+		
+//	SUBI:
+//		begin
+//			C = A - B;
+//			if (C == 16'b0000 0000 0000 0000)
+//				Flags[3] = 1'b1;
+//			else
+//				Flags[3] = 1'b0;
+//				
+//			if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) )
+//				Flags[2] = 1'b1;
+//			else
+//				Flags[2] = 1'b0;
+//			Flags[1:0] = 2'b00; Flags[4] = 1'b0;
+//		end
+		
+	//////////////////
+	CMP:
+		begin
+			//Flags = 5'b00000;
+			if(A == B)
+				Flags[1] = 1'b1;
+			else
+				Flags[1] = 1'b0;
+				
+			if( $signed(A) < $signed(B) ) 
+				Flags[0] = 1'b1;
+			else 
+				Flags[0] = 1'b0;
+		
+			Flags[3] = (A < B);
+			//C = 16'b0000 0000 0000 0000;
+			//Flags[4:2] = 3'b000;
+			
+			
+			
+			// both positive or both negative 
+//			if( A[15] == B[15] ) begin
+//				if (A < B) begin
+//					Flags[0] = 1'b1;
+//					Flags[3] = 1'b1;
+//				end
+//				else begin
+//					Flags[0] = 1'b0;
+//					Flags[3] = 1'b0;
+//				end
+//			end
+//			else if (A[15] == 1'b0)
+//				if (A < B) begin
+//					Flags[0] = 1'b1;
+//					Flags[3] = 1'b1;
+//				end
+//				
+//		// N is set if signed a is less than b
+//				else begin
+//					Flags[0] = 1'b0;
+//					Flags[3] = 1'b0;
+//				end
+				
+			Flags[2] = 1'b0;
+			Flags[4] = 1'b0;
+			
+			// C = ?? if I don;t specify, then I'm in trouble.
+			C = 16'b0000_0000_0000_0000;
+			
+		end
+//	CMPI:
+//		begin
+//		
+//		end
+//	CMPU:
+//		begin
+//		
+//		end
+		
+	//////////////////
+	AND:
+		begin
+			C = A & B;
+			Flags = 5'b0_0000;
+		end
+	OR:
+		begin
+			C = A | B;
+			Flags = 5'b0_0000;
+		end
+	XOR:
+		begin
+			C = A ^ B;
+			Flags = 5'b0_0000;
+		end
+	NOT:
+		begin
+			C = ~A;
+			Flags = 5'b0_0000;
+		end
+		
+	//////////////////
+	LSH:
+		begin
+			C = A << B;
+			Flags = 5'b0_0000;
+		end
+//	LSHI:
+//		begin
+//		
+//		end
+	RSH:
+		begin
+			C = A >> B;
+			Flags = 5'b0_0000;
+		end
+//	RSHI:
+//		begin
+//		
+//		end
+//	ALSH:
+//		begin
+//		
+//		end
+	ARSH:
+		begin
+			C = $signed(A) >>> B;
+			Flags = 5'b0_0000;
+		end
+//	WAIT:
+//		begin
+//			
+//		end
+		
+	default: 
+		begin
+			C = 16'b0000_0000_0000_0000;
+			Flags = 5'b0_0000;
+		end
+	endcase
+end
+
+endmodule
---
+++ Regfile ALU Datapath Lab 2/alutest.v
@@ -0,0 +1,899 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   14:25:01 08/30/2011
+// Design Name:   alu
+// Module Name:   C:/Documents and Settings/Administrator/ALU/alutest.v
+// Project Name:  ALU
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: alu
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module alutest;
+
+	// Inputs
+	reg [15:0] a;
+	reg [15:0] b;
+	reg [4:0] opcode;
+	reg cin;
+
+	// Outputs
+	wire [15:0] c;
+	wire [4:0] flags;
+
+	integer i;
+	// Instantiate the Unit Under Test (UUT)
+	alu uut (
+		.A(a), 
+		.B(b), 
+		.C(c), 
+		.Opcode(opcode), 
+		.Flags(flags),
+		.Cin(cin)
+	);
+
+	initial begin
+//			$monitor("A: %0d, B: %0d, C: %0d, Flags[1:0]:%b, time:%0d", A, B, C, Flags[1:0], $time );
+//Instead of the $display stmt in the loop, you could use just this
+//monitor statement which is executed everytime there is an event on any
+//signal in the argument list.
+
+		// Add
+		cin = 0;
+		opcode = 5'b00101;
+		a = 16'h0000;
+		b = 16'h0000;
+		
+		#10
+		
+		if(c == 16'h0000 && flags == 5'b00010)
+		begin
+			$display("Add 1 of 5 is chillin.");
+		end
+		else
+		begin
+			$display("Add 1 of 5 is not chillin.");
+		end
+		
+		
+		a = 16'h0402;
+		b = 16'h1325;
+		
+		#10
+		
+		if(c == 16'h1727 && flags == 5'b00000)
+		begin
+			$display("Add 2 of 5 is chillin.");
+		end
+		else
+		begin
+			$display("Add 2 of 5 is not chillin.");
+		end
+		
+		
+		a = 16'h0402;
+		b = 16'hf325;
+		
+		#10
+		
+		if(c == 16'hf727 && flags == 5'b00001)
+		begin
+			$display("Add 3 of 5 is chillin.");
+		end
+		else
+		begin
+			$display("Add 3 of 5 is not chillin.");
+		end
+		
+		
+		a = 16'h0402;
+		b = 16'hffff;
+		
+		#10
+		
+		if(c == 16'h0401 && flags == 5'b10000)
+		begin
+			$display("Add 4 of 5 is chillin.");
+		end
+		else
+		begin
+			$display("Add 4 of 5 is not chillin.  Should be 0401 and it is %h.  Flags should be 10100 but is %b", c, flags);
+		end
+		
+		
+		a = 16'h7fff;
+		b = 16'h0001;
+		
+		#10
+		
+		if(c == 16'h8000 && flags == 5'b00101)
+		begin
+			$display("Add 5 of 5 is chillin.");
+		end
+		else
+		begin
+			$display("Add 5 of 5 is not chillin.  Should be 8000 and it is %h.  Flags should be 00101 but is %b", c, flags);
+		end
+
+		// Addu
+
+		opcode = 5'b00110;
+		a = 16'h0000;
+		b = 16'h0000;
+		
+		#10
+		
+		if(c == 16'h0000 && flags == 5'b00000)
+		begin
+			$display("Addu 1 of 5 is chillin.");
+		end
+		else
+		begin
+			$display("Addu 1 of 5 is not chillin.");
+		end
+		
+		
+		a = 16'h0402;
+		b = 16'h1325;
+		
+		#10
+		
+		if(c == 16'h1727 && flags == 5'b00000)
+		begin
+			$display("Addu 2 of 5 is chillin.");
+		end
+		else
+		begin
+			$display("Addu 2 of 5 is not chillin.");
+		end
+		
+		
+		a = 16'h0402;
+		b = 16'hf325;
+		
+		#10
+		
+		if(c == 16'hf727 && flags == 5'b00000)
+		begin
+			$display("Addu 3 of 5 is chillin.");
+		end
+		else
+		begin
+			$display("Addu 3 of 5 is not chillin.");
+		end
+		
+		
+		a = 16'h0402;
+		b = 16'hffff;
+		
+		#10
+		
+		if(c == 16'h0401 && flags == 5'b00000)
+		begin
+			$display("Addu 4 of 5 is chillin.");
+		end
+		else
+		begin
+			$display("Addu 4 of 5 is not chillin.  Should be 0401 and it is %h.  Flags should be 00000 but is %b", c, flags);
+		end
+		
+		
+		a = 16'h7fff;
+		b = 16'h0001;
+		
+		#10
+		
+		if(c == 16'h8000 && flags == 5'b00000)
+		begin
+			$display("Addu 5 of 5 is chillin.");
+		end
+		else
+		begin
+			$display("Addu 5 of 5 is not chillin.  Should be 8000 and it is %h.  Flags should be 00000 but is %b", c, flags);
+		end
+
+		// Addc
+
+		opcode = 5'b00111;
+		a = 16'h0000;
+		b = 16'h0000;
+		
+		#10
+		
+		if(c == 16'h0000 && flags == 5'b00010)
+		begin
+			$display("Addc 1 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addc 1 of 10 is not chillin.");
+		end
+		
+		
+		a = 16'h0402;
+		b = 16'h1325;
+		
+		#10
+		
+		if(c == 16'h1727 && flags == 5'b00000)
+		begin
+			$display("Addc 2 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addc 2 of 10 is not chillin.");
+		end
+		
+		
+		a = 16'h0402;
+		b = 16'hf325;
+		
+		#10
+		
+		if(c == 16'hf727 && flags == 5'b00001)
+		begin
+			$display("Addc 3 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addc 3 of 10 is not chillin.");
+		end
+		
+		
+		a = 16'h0402;
+		b = 16'hffff;
+		
+		#10
+		
+		if(c == 16'h0401 && flags == 5'b10000)
+		begin
+			$display("Addc 4 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addc 4 of 10 is not chillin.  Should be 0401 and it is %h.  Flags should be 10100 but is %b", c, flags);
+		end
+		
+		
+		a = 16'h7fff;
+		b = 16'h0001;
+		
+		#10
+		
+		if(c == 16'h8000 && flags == 5'b00101)
+		begin
+			$display("Addc 5 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addc 5 of 10 is not chillin.  Should be 8000 and it is %h.  Flags should be 00101 but is %b.", c, flags);
+		end
+		
+		cin = 1;
+		a = 16'h0000;
+		b = 16'hffff;
+		
+		#10
+		
+		if(c == 16'h0000 && flags == 5'b10010)
+		begin
+			$display("Addc 6 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addc 6 of 10 is not chillin.  Should be 0000 and it is %h.  Flags should be 00010 but is %b.", c, flags);
+		end
+		
+		
+		a = 16'h0401;
+		b = 16'h1325;
+		
+		#10
+		
+		if(c == 16'h1727 && flags == 5'b00000)
+		begin
+			$display("Addc 7 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addc 7 of 10 is not chillin.");
+		end
+		
+		
+		a = 16'h0401;
+		b = 16'hf325;
+		
+		#10
+		
+		if(c == 16'hf727 && flags == 5'b00001)
+		begin
+			$display("Addc 8 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addc 8 of 10 is not chillin.");
+		end
+		
+		
+		a = 16'h0401;
+		b = 16'hffff;
+		
+		#10
+		
+		if(c == 16'h0401 && flags == 5'b10000)
+		begin
+			$display("Addc 9 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addc 9 of 10 is not chillin.  Should be 0401 and it is %h.  Flags should be 10100 but is %b", c, flags);
+		end
+		
+		
+		a = 16'h7ffe;
+		b = 16'h0001;
+		
+		#10
+		
+		if(c == 16'h8000 && flags == 5'b00101)
+		begin
+			$display("Addc 10 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addc 10 of 10 is not chillin.  Should be 8000 and it is %h.  Flags should be 00101 but is %b", c, flags);
+		end
+
+		// Addcu
+		
+		cin = 0;
+		opcode = 5'b01111;
+		a = 16'h0000;
+		b = 16'h0000;
+		
+		#10
+		
+		if(c == 16'h0000 && flags == 5'b00000)
+		begin
+			$display("Addcu 1 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addcu 1 of 10 is not chillin.");
+		end
+		
+		
+		a = 16'h0402;
+		b = 16'h1325;
+		
+		#10
+		
+		if(c == 16'h1727 && flags == 5'b00000)
+		begin
+			$display("Addcu 2 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addcu 2 of 10 is not chillin.");
+		end
+		
+		
+		a = 16'h0402;
+		b = 16'hf325;
+		
+		#10
+		
+		if(c == 16'hf727 && flags == 5'b00000)
+		begin
+			$display("Addcu 3 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addcu 3 of 10 is not chillin.");
+		end
+		
+		
+		a = 16'h0402;
+		b = 16'hffff;
+		
+		#10
+		
+		if(c == 16'h0401 && flags == 5'b00000)
+		begin
+			$display("Addcu 4 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addcu 4 of 10 is not chillin.  Should be 0401 and it is %h.  Flags should be 00000 but is %b", c, flags);
+		end
+		
+		
+		a = 16'h7fff;
+		b = 16'h0001;
+		
+		#10
+		
+		if(c == 16'h8000 && flags == 5'b00000)
+		begin
+			$display("Addcu 5 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addcu 5 of 10 is not chillin.  Should be 8000 and it is %h.  Flags should be 00000 but is %b", c, flags);
+		end
+
+		cin = 1;
+		a = 16'h0000;
+		b = 16'hffff;
+		
+		#10
+		
+		if(c == 16'h0000 && flags == 5'b00000)
+		begin
+			$display("Addcu 6 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addcu 6 of 10 is not chillin.");
+		end
+		
+		
+		a = 16'h0401;
+		b = 16'h1325;
+		
+		#10
+		
+		if(c == 16'h1727 && flags == 5'b00000)
+		begin
+			$display("Addcu 7 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addcu 7 of 10 is not chillin.");
+		end
+		
+		
+		a = 16'h0401;
+		b = 16'hf325;
+		
+		#10
+		
+		if(c == 16'hf727 && flags == 5'b00000)
+		begin
+			$display("Addcu 8 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addcu 8 of 10 is not chillin.");
+		end
+		
+		
+		a = 16'h0401;
+		b = 16'hffff;
+		
+		#10
+		
+		if(c == 16'h0401 && flags == 5'b00000)
+		begin
+			$display("Addcu 9 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addcu 9 of 10 is not chillin.  Should be 0401 and it is %h.  Flags should be 00000 but is %b", c, flags);
+		end
+		
+		
+		a = 16'h7ffe;
+		b = 16'h0001;
+		
+		#10
+		
+		if(c == 16'h8000 && flags == 5'b00000)
+		begin
+			$display("Addcu 10 of 10 is chillin.");
+		end
+		else
+		begin
+			$display("Addcu 10 of 10 is not chillin.  Should be 8000 and it is %h.  Flags should be 00000 but is %b", c, flags);
+		end
+		
+		// Sub
+		
+		opcode = 5'b01001;
+		a = 16'h7999;
+		b = 16'h5555;
+		
+		#10
+		
+		if(c == 16'h2444 && flags == 5'b00000)
+		begin
+			$display("Sub 1 of 6 is chillin.");
+		end
+		else
+		begin
+			$display("Sub 1 of 6 is not chillin.  Should be 2444 and it is %h.", c);
+		end
+		
+		
+		a = 16'h0002;
+		b = 16'h0001;
+		
+		#10
+		
+		if(c == 16'h0001 && flags == 5'b00000)
+		begin
+			$display("Sub 2 of 6 is chillin.");
+		end
+		else
+		begin
+			$display("Sub 2 of 6 is not chillin.  Should be 0001 and it is %h.", c);
+		end
+		
+		
+		a = 16'h9999;
+		b = 16'h5555;
+		
+		#10
+		
+		if(c == 16'h4444 && flags == 5'b00100)
+		begin
+			$display("Sub 3 of 6 is chillin.");
+		end
+		else
+		begin
+			$display("Sub 3 of 6 is not chillin.  Maybe should be 4444 and it is %h.  Flags should be 00100 but is %b.", c, flags);
+		end
+		
+		
+		a = 16'h7335;
+		b = 16'h7335;
+		
+		#10
+		
+		if(c == 16'h0000 && flags == 5'b00010)
+		begin
+			$display("Sub 4 of 6 is chillin.");
+		end
+		else
+		begin
+			$display("Sub 4 of 6 is not chillin.  Should be 0000 and it is %h.", c);
+		end
+		
+		
+		a = 16'h0005;
+		b = 16'h0006;
+		
+		#10
+		
+		if(c == 16'hffff && flags == 5'b00001)
+		begin
+			$display("Sub 5 of 6 is chillin.");
+		end
+		else
+		begin
+			$display("Sub 5 of 6 is not chillin.  Should be ffff and it is %h.  Flags should be 00001 but is %b", c, flags);
+		end
+		
+		
+		a = 16'h7fff;
+		b = 16'hfffe;
+		
+		#10
+		
+		if(c == 16'h8001 && flags == 5'b00101)
+		begin
+			$display("Sub 6 of 6 is chillin.");
+		end
+		else
+		begin
+			$display("Sub 6 of 6 is not chillin.  Should be 8001 and it is %h. Flags should be 00100 but are %b.", c, flags);
+		end
+		
+		// Cmp
+		
+		opcode = 5'b01011;
+		a = 16'h0001;
+		b = 16'h0000;
+		
+		#10
+		
+		if(c == 16'h0000 && flags == 5'b00000)
+		begin
+			$display("Cmp 1 of 5 is chilin.");
+		end
+		else
+		begin
+			$display("Cmp 1 of 5 is not chilin.");
+		end
+		
+		
+		a = 16'h0011;
+		b = 16'h0011;
+		
+		#10
+		
+		if(c == 16'h0000 && flags == 5'b00010)
+		begin
+			$display("Cmp 2 of 5 is chilin.");
+		end
+		else
+		begin
+			$display("Cmp 2 of 5 is not chilin.");
+		end
+		
+		
+		a = 16'h8001;
+		b = 16'h0002;
+		
+		#10
+		
+		if(c == 16'h0000 && flags == 5'b00001)
+		begin
+			$display("Cmp 3 of 5 is chilin.");
+		end
+		else
+		begin
+			$display("Cmp 3 of 5 is not chilin.  Flags is %b but should be 00001.", flags);
+		end
+		
+		
+		a = 16'h0301;
+		b = 16'h8000;
+		
+		#10
+		
+		if(c == 16'h0000 && flags == 5'b01000)
+		begin
+			$display("Cmp 4 of 5 is chilin.");
+		end
+		else
+		begin
+			$display("Cmp 4 of 5 is not chilin.  Flags is %b but should be 01000.", flags);
+		end
+		
+		
+		a = 16'h0001;
+		b = 16'h0300;
+		
+		#10
+		
+		if(c == 16'h0000 && flags == 5'b01001)
+		begin
+			$display("Cmp 5 of 5 is chilin.");
+		end
+		else
+		begin
+			$display("Cmp 5 of 5 is not chilin.");
+		end
+		
+		// Zero flag is set if they are the same 	// Tested
+		// L is set if unsigned a less than b		// Tested
+		// N is set if signed a is less than b		// Tested
+		
+		// And
+		
+		opcode = 5'b00001;
+		a = 16'h0007;
+		b = 16'h007f;
+		
+		#10 // Try without this wait.  //////////////////////////////////////////////////////////////////
+		
+		if(c == 16'h0007 && flags == 5'b00000)
+		begin
+			$display("And 1 of 2 is chilin.");
+		end
+		else
+		begin
+			$display("And 1 of 2 is not chilin.");
+		end
+		
+		
+		a = 16'hf00f;
+		b = 16'h0ff0;
+		
+		#10
+		
+		if(c == 16'h0000 && flags == 5'b00000)
+		begin
+			$display("And 2 of 2 is chilin.");
+		end
+		else
+		begin
+			$display("And 2 of 2 is not chilin.");
+		end
+		
+		// Or
+		
+		opcode = 5'b00010;
+		a = 16'h0007;
+		b = 16'h0070;
+		
+		#10 // Try without this wait.  //////////////////////////////////////////////////////////////////
+		
+		if(c == 16'h0077 && flags == 5'b00000)
+		begin
+			$display("Or 1 of 2 is chilin.");
+		end
+		else
+		begin
+			$display("Or 1 of 2 is not chilin.");
+		end
+		
+		
+		a = 16'hf00f;
+		b = 16'h0000;
+		
+		#10
+		
+		if(c == 16'hf00f && flags == 5'b00000)
+		begin
+			$display("Or 2 of 2 is chilin.");
+		end
+		else
+		begin
+			$display("Or 2 of 2 is not chilin.");
+		end
+		
+		// Xor
+		
+		opcode = 5'b00011;
+		a = 16'h0007;
+		b = 16'h0071;
+		
+		#10
+		
+		if(c == 16'h0076 && flags == 5'b00000)
+		begin
+			$display("Xor 1 of 2 is chilin.");
+		end
+		else
+		begin
+			$display("Xor 1 of 2 is not chilin.");
+		end
+		
+		
+		a = 16'hf00f;
+		b = 16'h0000;
+		
+		#10
+		
+		if(c == 16'hf00f && flags == 5'b00000)
+		begin
+			$display("Xor 2 of 2 is chilin.");
+		end
+		else
+		begin
+			$display("Xor 2 of 2 is not chilin.");
+		end
+		
+		// Not
+		
+		opcode = 5'b00100;
+		a = 16'hffff;
+		
+		#10
+		
+		if(c == 16'h0000 && flags == 5'b00000)
+		begin
+			$display("Not 1 of 2 is chillin.");
+		end
+		else
+		begin
+			$display("Not 1 of 2 is not chillin.");
+		end
+		
+		
+		a = 16'h0f0f;
+		
+		#10
+		
+		if(c == 16'hf0f0 && flags == 5'b00000)
+		begin
+			$display("Not 2 of 2 is chillin.");
+		end
+		else
+		begin
+			$display("Not 2 of 2 is not chillin.");
+		end
+		
+		// Lsh
+		
+		opcode = 5'b01100;
+		a = 16'hffff;
+		b = 16'h0004;
+		
+		#10
+		
+		if(c == 16'hfff0 && flags == 5'b00000)
+		begin
+			$display("Lsh 1 of 1 is chillin.");
+		end
+		else
+		begin
+			$display("Lsh 1 of 1 is not chillin.");
+		end
+		
+		
+		// Rsh
+		
+		opcode = 5'b10011;
+		a = 16'hffff;
+		b = 16'h0004;
+		
+		#10
+		
+		if(c == 16'h0fff && flags == 5'b00000)
+		begin
+			$display("Rsh 1 of 1 is chillin.");
+		end
+		else
+		begin
+			$display("Rsh 1 of 1 is not chillin.");
+		end
+		
+		// Arsh
+		
+		opcode = 5'b10111;
+		a = 16'hffff;
+		b = 16'h0004;
+		
+		#10
+		
+		if(c == 16'hffff && flags == 5'b00000)
+		begin
+			$display("Arsh 1 of 3 is chillin.");
+		end
+		else
+		begin
+			$display("Arsh 1 of 3 is not chillin.");
+		end
+		
+		
+		a = 16'h7fff;
+		b = 16'h0004;
+		
+		#10
+		
+		if(c == 16'h07ff && flags == 5'b00000)
+		begin
+			$display("Arsh 2 of 3 is chillin.");
+		end
+		else
+		begin
+			$display("Arsh 2 of 3 is not chillin.");
+		end
+		
+		
+		a = 16'h80ff;
+		b = 16'h0004;
+		
+		#10
+		
+		if(c == 16'hf80f && flags == 5'b00000)
+		begin
+			$display("Arsh 3 of 3 is chillin.");
+		end
+		else
+		begin
+			$display("Arsh 3 of 3 is not chillin.");
+		end
+		
+		$finish(2);
+
+	end
+
+endmodule
+
