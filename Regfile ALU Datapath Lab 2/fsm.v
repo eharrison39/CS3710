8f850520aac43c185c4093b84596e9f3be51a158
u1359199 <u1359199@gcloud.utah.edu>
2025-09-16 17:12:21 -0600

    Finished FSM Just need to put all components together in a top module, and finish test benches.

--- Regfile ALU Datapath Lab 2/fsm.v
+++ Regfile ALU Datapath Lab 2/fsm.v
@@ -13,43 +13,53 @@ module fsm(clk, rst, rs, rd, opcode, re, ri, fe, inop);
 	output reg [15:0] re;
 	output reg ri, fe;
 	
-	Reg [1:0]y;
+	reg [1:0]state;
 	parameter [1:0] S0 = 2'b00, S1 = 2'b01, S2 = 2'b10, S3 = 2'b11;
 	
 	always @(posedge clk or negedge rst) begin
 	 if (~rst)
 		state <= S0;
 	 else
-		case(y)
-			S0: y <= S1
-			S1: y <= S2
-			S2: y <= S3
-			S3: y <= S3
-			default: y <= S0
+		case(state)
+			S0: state <= S1;
+			S1: state <= S2;
+			S2: state <= S3;
+			S3: state <= S3;
+			default: state <= S0;
 		endcase
 	end
 			
-	always@ (y)
+	always@ (state) begin
 		S0:
 			begin
 				re = 16'h0000;
-				rs = rd = 4'h0;
+				rs = 4'h0;
+				rd = 4'h0;
 				opcode = 5'b0_0000;
-				ri = fe = 1'b0;
+				ri = 1'b0;
+				fe = 1'b0;
 			end
 		S1:
 			begin
 				re = 16'h0000;
+				
+				// Set rd and rs muxes
 				rd = inop[11:8];
 				rs = inop[3:0];
+				
+				// Open source and dest registers
+				re[rd] = 1;
+				
 				if(inop[15:12] == 4'h0) begin
-					ri = 0;
 					re[rs] = 1;
-					re[rd] = 1;
+					
+					// Set rimmediate mux control in.
+					ri = 0;
 				end
 				else
 					ri = 1;
-				
+					
+				// Set alu opcode
 				if(inop[15:12] == 4'h0)
 					opcode = 0 & inop[7:4];
 				else if(inop[15:12] == 4'b1000 && inop[7:4] == 4'b0100)
@@ -62,27 +72,31 @@ module fsm(clk, rst, rs, rd, opcode, re, ri, fe, inop);
 					opcode = 5'b10111;
 				else
 					opcode = 0 & inop[15:12];
+					
+				// Enable alu flags
+				fe = 1;
 			
 			end		
 		S2:
+			begin
+				re = 16'h0000;
+				re[rd] = 1;
+				rs = 4'h0;
+				rd = 4'h0;
+				opcode = 5'b0_0000;
+				ri = 1'b0;
+				fe = 1'b0;
+			end
 		S3:
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+			begin
+				re = 16'h0000;
+				rs = 4'h0;
+				rd = 4'h0;
+				opcode = 5'b0_0000;
+				ri = 1'b0;
+				fe = 1'b0;
+			end	
+	end	
 			
 endmodule
 
