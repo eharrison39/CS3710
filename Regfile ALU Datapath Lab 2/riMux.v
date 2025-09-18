9de71dbf88297f61f432a9ddf00d218d90a7f632
u1359199 <u1359199@gcloud.utah.edu>
2025-09-18 16:39:15 -0600

    Fix riMux

--- Regfile ALU Datapath Lab 2/riMux.v
+++ Regfile ALU Datapath Lab 2/riMux.v
@@ -3,94 +3,24 @@
 // @Authors Luke Jones, David Brown, Elden Harrison
 // Mux module, 16 registers as input with a 4 bit opcode as the control line
 // look into an actual verilog mux
-module mux (rsrc, imm, ri, out);
+module riMux (rsrc, imm, ri, out);
 	
-	input [1:0] ri;
+	input ri;
 	input [15:0] rsrc, imm;
 	output reg[15:0] out;
 	
-always @(r, r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15)
+always @(ri, rsrc, imm)
 begin
-	case (r)
+	case (ri)
 	
-	4'b0000:
+	1'b0:
 		begin
-			out = r0;
+			out = rsrc;
 		end
 		
-	4'b0001:
+	1'b1:
 		begin
-			out = r1;
-		end
-		
-	4'b0010:
-		begin
-			out = r2;
-		end
-	
-	4'b0011:
-		begin
-			out = r3;
-		end
-	
-	4'b0100:
-		begin
-			out = r4;
-		end
-		
-	4'b0101:
-		begin
-			out = r5;
-		end
-		
-	4'b0110:
-		begin
-			out = r6;
-		end
-		
-	4'b0111:
-		begin
-			out = r7;
-		end
-		
-	4'b1000:
-		begin
-			out = r8;
-		end
-		
-	4'b1001:
-		begin
-			out = r9;
-		end
-		
-	4'b1010:
-		begin
-			out = r10;
-		end
-	
-	4'b1011:
-		begin
-			out = r11;
-		end
-	
-	4'b1100:
-		begin
-			out = r12;
-		end
-		
-	4'b1101:
-		begin
-			out = r13;
-		end
-		
-	4'b1110:
-		begin
-			out = r14;
-		end
-		
-	4'b1111:
-		begin
-			out = r15;
+			out = imm;
 		end
 	endcase
 end
