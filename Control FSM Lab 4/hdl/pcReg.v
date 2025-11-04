b97d2ec8a881ec7232e5f460e9a096810cf139d4
umyea33 <143153983+umyea33@users.noreply.github.com>
2025-11-04 14:40:53 -0700

    Pc Reg

---
+++ Control FSM Lab 4/hdl/pcReg.v
@@ -0,0 +1,24 @@
+
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 		Luke Jones
+// 
+// Create Date:    10/30/2025
+// Design Name: 
+// Module Name:    flag reg
+// Revision: 		
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+module pcReg(in, pcEn, reset, clk, out);
+input [15:0] in;
+input pcEn, reset, clk;
+output reg [15:0] out;
+
+
+(* preserve, keep *)register thePcReg(in, pcEn, reset, clk, out);
+
+endmodule
